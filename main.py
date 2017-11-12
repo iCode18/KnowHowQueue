e431d5b520f1112b0803931138a661e42b6e8c2d
iCode18 <alavi.afshin@gmail.com>
2017-11-12 20:11:50 +0100

    did some changes in design and added events

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+from collections import deque
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 
 class QueueCanvas(BoxLayout):
-    pass
+    customers = 0
+    customersName = deque([])
 
+    def label_update(self):
+        self.ids['Customer'].text = str(self.customers) + ' står i køen'
+
+    def customer_register(self):
+        self.customersName.append(self.ids['name_register'].text)
+        self.ids['name_register'].text = ' '
 
 class QueueApp(App):
     def build(self):
